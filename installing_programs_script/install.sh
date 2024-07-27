08b3353771fbf5c762ab1343fc751374889e47cc
KosenkoAlexander <alexandkosenko@gmail.com>
2024-07-27 14:46:25 +0300

    Main programs added to install.sh

--- installing_programs_script/install.sh
+++ installing_programs_script/install.sh
@@ -3,9 +3,23 @@
 # +-----------------------------+
 # | Installation configurations |
 # +-----------------------------+
+
+# Main
+install_office = true
+install_kde = true
+install_dev_tools = true
+install_flatpak = false # if user has no flatpak yet; requires restart
+install_flatpaks = true
+install_aurs = true
+install_zsh = true
+install_yay = false
+#install_powerlevel10k_zsh_theme = false # done as separate script; requires zsh and yay, also reboot before doing it or run zsh config in any other way
+
+# KDE
 install_chemistry = true
 install_teatime = true
 install_graphical_design = true
+install_virtualbox = true
 
 # dev_tools
 install_java = true # java's probably too popular to be optional
@@ -15,7 +29,6 @@ install_python = true
 
 # Conditional Flatpak applications
 install_spotify=false
-install_slack=false
 
 browsers=(
   [chromium] = true
@@ -32,6 +45,7 @@ messengers=(
   [whatsapp] = false
   [discord]  = true
   [zoom]     = true
+  [slack]    = true
 )
 
 # helper variables
@@ -41,6 +55,16 @@ browser_flatpak=(
   [vivaldi]  = "com.vivaldi.Vivaldi"
   [brave]    = "com.brave.Browser"
   [opera]    = "com.opera.Opera"
+  [tor]      = "org.torproject.torbrowser-launcher"
+)
+
+messengers_flatpack=(
+  [telegram] = "org.telegram.desktop"
+  [signal]   = "org.signal.Signal"
+  [whatsapp] = "chat.rocket.RocketChat"
+  [discord]  = "chat.rocket.RocketChat"
+  [zoom]     = "com.discordapp.Discord"
+  [slack]    = "com.slack.Slack"
 )
 
 function add_to_list_by_dict_condition() { # args: 1 - condition dict, 2 - to add dict, 3 - list for addition
@@ -49,6 +73,8 @@ function add_to_list_by_dict_condition() { # args: 1 - condition dict, 2 - to ad
       $3 += (
         "${$2[$element]}"
       )
+    else
+      echo "No script for $element installation"
     fi
   done
 } # not sure if works
@@ -67,7 +93,7 @@ function is_in_list {
 install_packages() {
   local packages=("$@")
   for package in "${packages[@]}"; do
-    sudo pacman -S --noconfirm "$package"
+    sudo pacman -S --noconfirm --needed "$package"
   done
 }
 
@@ -161,7 +187,7 @@ fi
 
 
 flatpaks=(
-  org.gimp.GIMP                  # GIMP image editor
+  #org.gimp.GIMP                  # GIMP image editor
   io.mpv.Mpv                     # MPV media player
   com.obsproject.Studio          # OBS Studio
   com.valvesoftware.Steam        # Steam
@@ -169,66 +195,93 @@ flatpaks=(
 )
 
 # Add browsers to flatpaks list if specified
-for browser in "${browsers[@]}"; do
-  case $browser in
-    chromium)
-      flatpaks+=("org.chromium.Chromium")
-      ;;
-    firefox)
-      flatpaks+=("org.mozilla.firefox")
-      ;;
-    vivaldi)
-      flatpaks+=("com.vivaldi.Vivaldi")
-      ;;
-    brave)
-      flatpaks+=("com.brave.Browser")
-      ;;
-    tor)
-      flatpaks+=("org.torproject.torbrowser-launcher")
-      ;;
-    opera)
-      flatpaks+=("com.opera.Opera")
-      ;;
-    *)
-      echo "Unknown browser: $browser"
-      ;;
-  esac
-done
+#for browser in "${browsers[@]}"; do
+#  case $browser in
+#    chromium)
+#      flatpaks+=("org.chromium.Chromium")
+#      ;;
+#    firefox)
+#      flatpaks+=("org.mozilla.firefox")
+#      ;;
+#    vivaldi)
+#      flatpaks+=("com.vivaldi.Vivaldi")
+#      ;;
+#    brave)
+#      flatpaks+=("com.brave.Browser")
+#      ;;
+#    tor)
+#      flatpaks+=("org.torproject.torbrowser-launcher")
+#      ;;
+#    opera)
+#      flatpaks+=("com.opera.Opera")
+#      ;;
+#    *)
+#      echo "Unknown browser: $browser"
+#      ;;
+#  esac
+#done
 
-# Add messengers to flatpaks list if specified
-for messenger in "${messengers[@]}"; do
-  case $messenger in
-    telegram)
-      flatpaks+=("org.telegram.desktop")
-      ;;
-    signal)
-      flatpaks+=("org.signal.Signal")
-      ;;
-    whatsapp)
-      flatpaks+=("chat.rocket.RocketChat")
-      ;;
-    discord)
-      flatpaks+=("com.discordapp.Discord")
-      ;;
-    zoom)
-      flatpaks+=("us.zoom.Zoom")
-      ;;
-    *)
-      echo "Unknown messenger: $messenger"
-      ;;
-  esac
-done
+add_to_list_by_dict_condition "$browsers" "$browser_flatpak" "$flatpaks"
+
+add_to_list_by_dict_condition "$messengers" "$messengers_flatpack" "$flatpaks"
+
+## Add messengers to flatpaks list if specified
+#for messenger in "${messengers[@]}"; do
+#  case $messenger in
+#    telegram)
+#      flatpaks+=("org.telegram.desktop")
+#      ;;
+#    signal)
+#      flatpaks+=("org.signal.Signal")
+#      ;;
+#    whatsapp)
+#      flatpaks+=("chat.rocket.RocketChat")
+#      ;;
+#    discord)
+#      flatpaks+=("com.discordapp.Discord")
+#      ;;
+#    zoom)
+#      flatpaks+=("us.zoom.Zoom")
+#      ;;
+#    slack)
+#      flatpaks+=("com.slack.Slack")
+#      ;;
+#    *)
+#      echo "Unknown messenger: $messenger"
+#      ;;
+#  esac
+#done
+
+if $install_graphical_design; then
+  flatpaks+=("org.inkscape.Inkscape" "org.gimp.GIMP")
+fi
 
 # Add Spotify and Slack if flags are set
 if $install_spotify; then
   flatpaks+=("com.spotify.Client")
 fi
 
-if $install_slack; then
-  flatpaks+=("com.slack.Slack")
+aurs=(
+  htop
+  neofetch
+  #flatpak
+  which
+  wine
+  vim
+  #virtualbox
+)
+
+if $install_virtualbox; then
+  aurs += (virtualbox)
 fi
 
+if $install_flatpak; then
+  aurs += (flatpak)
+fi
 
+office=(
+  libreoffice-fresh
+)
 
 # Update the package database and upgrade all packages
 sudo pacman -Syu
@@ -236,16 +289,44 @@ sudo pacman -Syu
 # +--------------------------+
 # | Install KDE applications |
 # +--------------------------+
-
-install_packages "${kde_apps[@]}"
-
+if $install_kde; then
+  install_packages "${kde_apps[@]}"
+fi
 # +---------------------------+
 # | Install development tools |
 # +---------------------------+
 
-install_packages "${dev_tools[@]}"
+if $install_dev_tools; then
+  install_packages "${dev_tools[@]}"
+fi
 
-install_flatpaks "${flatpaks[@]}"
+if $install_aurs; then
+  install_packages "${aurs[@]}"
+fi
+
+if $install_office; then
+  install_packages "${office[@]}"
+fi
+
+if $install_flatpaks; then
+  install_flatpaks "${flatpaks[@]}"
+fi
 
 echo "KDE package list have been installed."
+
+# +--------------------------+
+# | Install and decorate zsh |
+# +--------------------------+
+
+if $install_zsh; then
+  pacman -S --noconfirm --needed zsh
+  chsh -s $(which zsh)
+fi
+
+if $install_yay; then
+  pacman -S --noconfirm --needed base-devel
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si --noconfirm
+fi
 
