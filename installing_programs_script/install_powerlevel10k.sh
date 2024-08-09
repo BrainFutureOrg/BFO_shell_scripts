b7af9c4e0cc74d391ab3727e67799894e5d22c26
KosenkoAlexander <70208007+KosenkoAlexander@users.noreply.github.com>
2024-08-09 22:40:56 +0300

    deleted accidental symbol in install_powerlevel10k.sh

--- installing_programs_script/install_powerlevel10k.sh
+++ installing_programs_script/install_powerlevel10k.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yay -Sy --noconfirm ttf-meslo-nerd-font-powerlevel10k zsh-theme-powerlevel10k-git
-echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
